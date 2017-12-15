158c506efbe32670768cbea0dd5df58a05ac39b4
Gwen <gwenael.leroutier@gmail.com>
2017-12-15 14:48:15 +0100

    commit test

--- app/components/SpeciesList/SpecieList/SpecieList.js
+++ app/components/SpeciesList/SpecieList/SpecieList.js
@@ -6,15 +6,41 @@ import { Tabs, Tab } from 'react-bootstrap'
 class SpecieList extends React.Component {
 
     componentDidMount() {
-
+    
     }
 
     render() {
+       
+      let species = this.props.speciesList
 
+      const list = []
+
+      for (let specie in species){
+          console.log(species[specie].specieName)
+
+          let specieData = {
+              SpecieName: species[specie].specieName,
+              SpeciePhotoProfil: species[specie].SpeciePhotoProfil,
+              SpecieId: species[specie].SpecieId
+          };
+
+          list.push(specieData);
+
+          console.log(list)
+      }
+
+      
         return (
             <div>
+
                 {
-                    this.props.myList.map(function (specie) { return <SpecieWidget specieData={specie} />; })
+                    list.map(function (specie) { return <SpecieWidget specieData={specie} />; })
+                }
+
+
+
+                {
+                   // this.props.myList.map(function (specie) { return <SpecieWidget specieData={specie} />; })
                 }
             </div>
 
